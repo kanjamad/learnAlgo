bce7e9a61e71f0efaf54200e198ec9ef0824fc71
kanjamad bosel <kanjamadpor@gmail.com>
2020-06-06 14:03:40 -0700

    solving Insert first

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -9,9 +9,51 @@ class Node {
     }
 }
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    insertFirst(data) {
+        const node = new Node(data, this.head);
+        this.head = node;
+    }
+}
 
 module.exports = {
     Node,
     LinkedList
 };
+
+/*
+Example
+
+
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    insertFirst(data) {
+        const node = new Node(data, this.head);
+        this.head = node;
+    }
+}
+
+const nodeOne = new Node(5);
+const list = new LinkedList();
+list.head = nodeOne;
+list.insertFirst(15);
+list
+
+
+result:
+{"data":5,"next":null}
+{"head":{"data":15,"next":{"data":5,"next":null}}}
+
+*/
