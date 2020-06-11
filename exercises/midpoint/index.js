c3df2bc33a4c25f50ef4a1fcf9b7846c7264f599
kanjamad bosel <kanjamadpor@gmail.com>
2020-06-11 11:57:02 -0700

    midpoint

--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -12,6 +12,24 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-function midpoint(list) {}
+function midpoint(list) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+}
 
 module.exports = midpoint;
+
+/*
+   Head            Tail
+  ____ ___        _____ ___         
+| 123 | * |  --> | 456 | * | -->
+  ---  ---        ----- ---
+ data  Next       data  Next
+
+*/
